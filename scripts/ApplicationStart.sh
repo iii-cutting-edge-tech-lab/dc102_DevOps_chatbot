7ea7f38304b446305649e28e1ec03f655b406edd
encorec00000 <encorec00000@gmail.com>
2019-08-03 14:50:29 +0800

    2

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -p 5000:5000 --name flask-devops -d 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/tibame-flask01
+docker run -p 5000:5000 --name flask-devops -d 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/devops-test
--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -5,7 +5,7 @@ $(aws ecr get-login --no-include-email --registry-ids 204065533127  --region ap-
 if [ `docker images | grep flask-devops | wc -l`  = 1 ]
 then
         docker rmi flask-devops
-        docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/tibame-flask01
+        docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/devops-test
 else
-        docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/tibame-flask01
+        docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/devops-test
 fi
