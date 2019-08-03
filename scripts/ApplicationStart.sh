56395d0838ee6b6dec9068364417539c85c75453
encorec00000 <encorec00000@gmail.com>
2019-08-03 14:04:10 +0800

    1

---
+++ scripts/ApplicationStart.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -p 5000:5000 --name flask-devops -d 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/tibame-flask01
---
+++ scripts/BeforeInstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+$(aws ecr get-login --no-include-email --registry-ids 204065533127  --region ap-northeast-1)
+
+if [ `docker images | grep flask-devops | wc -l`  = 1 ]
+then
+        docker rmi flask-devops
+        docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/tibame-flask01
+else
+        docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/tibame-flask01
+fi
