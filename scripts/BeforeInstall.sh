fa8b0b9ac584e812659695f2a4b81bee5e279d5d
reply9746 <reply9746@gmail.com>
2019-08-04 13:54:31 +0800

    v2

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -1,12 +1,11 @@
-
 #!/bin/bash
 #登入ECR repo
 $(aws ecr get-login --no-include-email --registry-ids 204065533127 --region ap-northeast-1)
 #依據docker image數量刪除EC2內的image檔，並從ECR內下載最新版本的image檔
 if [ `docker images | wc -l` > 1 ]
 then
-        docker image rm -f 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/devops-test:latest
+        docker image rm -f 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/devops-v1:latest
 else
-        docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/devops-test:latest
+        docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/devops-v1:latest
 fi
 exit 0
